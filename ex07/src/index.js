669c7dc79b0f93928ce28ec83e2c3bac5e3b4726
gogi999 <g.petrovic9@gmail>
2021-03-23 18:18:57 +0100

    Added ex07/src/index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,4 @@
+const person = require("./nameFunctions.js");
+
+// console.log(`${person.firstName("Joe")} ${person.lastName("Doe")}`);
+console.log(person.firstName("Joe") + " " + person.lastName("Doe"));
---
+++ ex07/src/nameFunctions.js
@@ -0,0 +1,12 @@
+const firstName = (firstName) => {
+    return firstName.toUpperCase();
+}
+
+const lastName = (lastName) => {
+    return lastName.toLowerCase();
+}
+
+module.exports = {
+    firstName,
+    lastName
+}
