765a3d9d49c83be7681185deab3a7b8f84b22ff2
gogi999 <g.petrovic9@gmail>
2021-03-23 18:32:12 +0100

    Added ex08/src/index.js

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor(name, email, age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    getPerson() {
+        const personInfo = `Name: ${this.name}, email: ${this.email}, age: ${this.age}`;
+        return personInfo;
+    }
+}
+
+const person = new Person("Goran", "gogi9@email.com", 47);
+console.log(person.getPerson());
+
+module.exports = Person;
